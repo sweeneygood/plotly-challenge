e51d552038bcb433c60af1a808d24364b43853a3
sweeneygood <sweeneygood@gmail.com>
2021-12-26 21:39:57 -0600

    updated demo info and bubble chart

--- plots.js
+++ plots.js
@@ -9,7 +9,6 @@ function InitDashboard() {
     //loop to populate the drop down of the names from the datafile
     d3.json("samples.json").then(data => {
     
-        //console.log(data);
         let sampleNames = data.names;
 
         sampleNames.forEach(sampleId => {
@@ -31,15 +30,12 @@ function optionChanged(id) {
     console.log(id);
 
     //populate the demographic info
-
     populateDemographics(id);
 
     // draw the bar chart 
-
     drawBarChart(id);
 
     // draw the bubble chart 
-
     drawBubbleChart(id);
 
 
@@ -47,7 +43,6 @@ function optionChanged(id) {
 
 function drawBarChart (sampleId) {
 
-    //console.log("The value for id is " + sampleId); 
 
     d3.json("samples.json").then(data => {
 
@@ -86,31 +81,26 @@ function drawBarChart (sampleId) {
 function populateDemographics (sampleId) {
 
     console.log(`populateDemographics`);
-    // d3.json("samples.json").then(data => {
 
-    //     let samples = data.samples; 
-    //     let resultArray = samples.filter(s => s.id === sampleId); 
-    //     let result = resultArray[0];
-    //     console.log(result);
+    d3.json("samples.json").then(data => {
 
-    //     let otu_ids = result.otu_ids;
-    //     let otu_labels = result.otu_labels;
-    //     let sample_values = result.sample_values;
-    //     let yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}` );
-        
+        let metadata = data.metadata; 
 
-    //     let barData = [{
-    //         x: sample_values.slice(0,10),
-    //         y: yticks,
-    //         type: "bar",
-    //         text: otu_labels.slice(0,10),
-    //         orientation: "h"
+        console.log('metadata');
+        console.log(metadata);
+        let resultArray = metadata.filter(s => s.id === parseInt(sampleId)); 
+        let result = resultArray[0];
 
-    //     }]
+        var demoData = d3.select("#sample-metadata");
 
-    //     Plotly.newPlot("bar", barData); 
+        // Clear the existing data 
+        demoData.html("");
 
-    // });
+        Object.entries(result).forEach(([key, value]) => {
+            demoData.append("h6").text(`${key}: ${value}`);
+        });
+
+    });
 
 
 }
@@ -119,6 +109,39 @@ function populateDemographics (sampleId) {
 function drawBubbleChart (sampleId) {
 
     console.log(`drawBubbleChart`);
+
+    d3.json("samples.json").then(data => {
+
+        let samples = data.samples; 
+        let resultArray = samples.filter(s => s.id === sampleId); 
+        let result = resultArray[0];
+
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
+        let sample_values = result.sample_values;
+        
+        var trace1 = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: 'markers',
+            marker: {
+                color: otu_ids,
+                size: sample_values
+            }
+        };
+  
+        var data = [trace1];
+  
+        var layout = {
+            title: 'Bacteria Cultures Per Sample',
+            showlegend: false
+        };
+
+    Plotly.newPlot('bubble', data, layout);
+
+    });
+
 }
 
 
